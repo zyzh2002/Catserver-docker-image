0abf36b24ee0eea3626510a92727675dc6de2da8
zyzh2002 <zyzh02@live.com>
2021-02-16 21:43:02 +0800

    modified

--- download.py
+++ download.py
@@ -2,5 +2,8 @@ import requests
 import json
 
 def getAddress():
-    restlt = requests.get("https://api.github.com/repos/Luohuayu/Catserver/releases/latest").text
-    
+    result = requests.get("https://api.github.com/repos/Luohuayu/Catserver/releases/latest").text
+    data = json.loads(str(result, encoding='utf-8'))
+    data = data['assets']
+    data = data['browser_download_url']
+    print(data)
