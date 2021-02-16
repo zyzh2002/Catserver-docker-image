d1fe74a241eae264fc98d0c94dd72198afa93707
zyzh2002 <zyzh02@live.com>
2021-02-16 22:20:17 +0800

    modified

--- download.py
+++ download.py
@@ -3,7 +3,14 @@ import json
 
 def getAddress():
     result = requests.get("https://api.github.com/repos/Luohuayu/Catserver/releases/latest").text
-    data = json.loads(str(result, encoding='utf-8'))
+    data = json.loads(str(result))
     data = data['assets']
+    data = data[0]
     data = data['browser_download_url']
     print(data)
+
+def downloadFile(url):
+    import wget
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+    wget.download(url)
